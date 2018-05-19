4280787efdb61038a029902ea68b5f29d0ea8ff9
Lazy-Y <zhenyanz@usc.edu>
2018-05-19 10:34:14 -0700

    Add key

--- authserver/settings/dev.py
+++ authserver/settings/dev.py
@@ -6,3 +6,4 @@ SECRET_KEY = 'b9rcz^eu7sx%rs3%e=wu_s!gs4)a7p42!=kuj)bbfva1v0d^ju'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 JWT_AUTH['JWT_PRIVATE_KEY'] = open('authserver/settings/dev.key').read()
+JWT_AUTH['JWT_PUBLIC_KEY'] = open('authserver/settings/dev.key.pub').read()
